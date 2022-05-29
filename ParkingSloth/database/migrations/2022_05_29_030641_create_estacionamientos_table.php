4de73c29b8e85b96ee82ea37f0d681f55999a3b1
Foorajido <felipe.valenzuela1801@alumnos.ubiobio.cl>
2022-05-28 23:44:39 -0400

    Creacion Modelo/Controlador Estacionamiento

---
+++ ParkingSloth/database/migrations/2022_05_29_030641_create_estacionamientos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estacionamientos', function (Blueprint $table) {
+            $table->id('ID_Estacionamiento')->autoIncrement();
+            $table->integer('Numero');
+            $table->boolean('Activo');
+            $table->integer('Capacidad_Total');
+            $table->integer('Capacidad_Utilizada');
+            $table->string('Referencia')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estacionamientos');
+    }
+};
---
+++ ParkingSloth/resources/views/test.php
@@ -0,0 +1,5 @@
+<div class="container-fluid">
+    <div class="container">
+        <h1>Test</h1>
+    </div>
+</div>
