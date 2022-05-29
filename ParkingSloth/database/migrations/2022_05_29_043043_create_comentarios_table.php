e169f0b739d3022a62927c823c7a35852546abd0
Carlos <carlos.aip212@gmail.com>
2022-05-29 00:33:55 -0400

    Make model -mcr Reporte/Comentarios

---
+++ ParkingSloth/database/migrations/2022_05_29_043043_create_comentarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->id('ID_Comentario');
+            $table->integer('ID_Usuario')->references('ID_Usuario')->on('Usuario');
+            $table->string('Titulo');
+            $table->string('Mensaje');
+            $table->integer('Clasificacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comentarios');
+    }
+};
