bc9552eb984ee57a9c802f697b98e6d417b9ffa9
AaronMV <aronmateo13@gmail.com>
2022-06-01 13:10:12 -0400

    cambios en general de actualizar

--- ParkingSloth/resources/views/ActualizarEstacionamientos/Main.blade.php
+++ ParkingSloth/resources/views/ActualizarEstacionamientos/Main.blade.php
@@ -3,8 +3,49 @@
 @section('content')
 
 <div class="" style="text-align: center;margin-top:5%">
+<h1 class="text-center text-uppercase">Actualizacion Estacionamiento</h1>
+</div>
+
+<div class="" style="text-align: center">
  <span id="clock"></span>
 </div>
+
+<hr> 
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 ">
+					<div class="info-card info-card--success">
+						<div class="info-card_icon">
+
+						<img width="100" height="100"  src="https://static.vecteezy.com/system/resources/previews/004/209/792/non_2x/traffic-light-signal-free-vector.jpg" >
+						
+					</div>
+						<h2 class="info-card_label">Calle</h2>
+						<div class="info-card_message">.sdasdasd</div>
+					</div>
+				</div>
+				<div class="col-sm-6 ">
+					<div class="info-card info-card--warning">
+						<div class="info-card_icon">
+
+						<img width="70" height="70" src="https://media.istockphoto.com/vectors/parking-signal-vector-id504033151?b=1&amp;k=6&amp;m=504033151&amp;s=170667a&amp;w=0&amp;h=a3waZmmpz0Khe6eYBs53l4HnS8XOreRiLGulHOzhDoA=">
+
+						</div>
+						<h2 class="info-card_label">Total Estacionamiento</h2>
+						<div class="info-card_message">asdasd</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<hr> 
+
+<div class="" style="text-align: center">
+<h1 >Actualizar</h5>
+<h6 >"Cantidad Actual de estacionamiento"</h6>
+
+</div>
+
 <div class="center">
 	<div class="containerR">
 		<button id="decrement" onclick="stepper(this)" class = "buttoninde">-</button>
@@ -13,6 +54,8 @@
 		<button id="increment" onclick="stepper(this)" class = "buttoninde">+</button>
 	</div>
 </div>
+
+
 
 
 
@@ -102,6 +145,73 @@ input::-webkit-inner-spin-button {
 	padding: 15px 25px 15px 5px;
 	border-radius: 0 45px 45px 0;
 }
+
+
+
+
+
+
+.main-content {
+	padding-top: 100px;
+	padding-bottom: 100px;
+}
+
+.info-card {
+	background: #fff;
+	text-align: center;
+	margin-bottom: 30px;
+	border-radius: 3px;	
+	box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
+}
+.info-card .info-card_icon {
+	height: 125px;
+	width: 125px;
+	margin: 0 auto 0px auto;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+}
+.info-card .info-card_icon i {
+	font-size: 50px;
+	color: #4caf50;
+}
+.info-card .info-card_icon .info-card_img-icon {
+	height: 60px;
+	width: 60px;
+	object-fit: contain;
+}
+.info-card .info-card_label {
+	margin-bottom: 15px;
+}
+.info-card .info-card_message {
+	margin-bottom: 15px;
+}
+.info-card .btn {
+	background: #03a9f4;
+	border-color: #03a9f4;
+	box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1);
+}
+.info-card--success .info-card_icon {
+	border-color: #4caf50;
+}
+.info-card--success .info-card_icon i {
+	color: #4caf50;
+}
+.info-card--danger .info-card_icon {
+	border-color: #f44336;
+}
+.info-card--danger .info-card_icon i {
+	color: #f44336;
+}
+.info-card--warning .info-card_icon {
+	border-color: #ff9800;
+}
+.info-card--warning .info-card_icon i {
+	color: #ff9800;
+}
+
+
 
 </style>
 
