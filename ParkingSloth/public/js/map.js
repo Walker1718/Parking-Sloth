369852263b766148e81789654a9e763aeae6b061
Elinksnow <elizabeth.elcl@gmail.com>
2022-08-18 22:35:53 -0500

    creacion infowindow en mapa

--- ParkingSloth/public/js/map.js
+++ ParkingSloth/public/js/map.js
@@ -1,4 +1,29 @@
 let map;
+let markers=[];
+
+const createMarker = (coord,name)=>{
+    let html = `<h5>${name}</h5>`
+    const marker = new google.maps.Marker({
+        position: coord,
+        map:map,
+        // icon: "./img/logo.png"
+    })
+    google.maps.event.addListener(marker, "click", ()=>{
+        infoWindow.setContent(html);
+        infoWindow.open(map, marker)
+    })
+
+    markers.push(marker)
+}
+
+
+const createLocationMarkers = ()=>{
+    estacionamientos.forEach(estacionamientos=>{
+        let coord = new google.maps.LatLng(estacionamientos.lat, estacionamientos.lng);
+        let name = estacionamientos.name;
+        createMarker(coord, name);
+    })
+}
 
 function initMap() {
     let concepcion = { lat: -36.82699, lng: -73.04977 }
@@ -8,7 +33,10 @@ function initMap() {
     zoom: 13,
     });
 
-    console.log(estacionamientos)
+   createLocationMarkers()
+
+   infoWindow = new google.maps.InfoWindow();
+   let html = `<h5>Prueba prueba</h5>`
 }
 
 // window.initMap = initMap;
