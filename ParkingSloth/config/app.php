7e1fc980cef87b0e6e7b1f895fa39573ab4eb2d2
Ailyn Cofré <ailyn.cofre1601@alumnos.ubiobio.cl>
2022-08-20 22:47:11 -0400

    agregar idioma español

--- ParkingSloth/config/app.php
+++ ParkingSloth/config/app.php
@@ -82,7 +82,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
@@ -185,7 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Laraveles\Spanish\SpanishServiceProvider::class,
         /*
          * Application Service Providers...
          */
